0ed9d3b367e156ce0c40566e3bd884d0be30c9a7
Sean <sean.n.cordes@gmail.com>
2018-10-23 15:31:25 -0400

    added validation testing

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,8 @@
 class Venue < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: :true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :business_type, presence: true
+  validates :age_of_entry, presence: true
 end
--- test/models/venue_test.rb
+++ test/models/venue_test.rb
@@ -1,5 +1,29 @@
 require 'test_helper'
 
 class VenueTest < ActiveSupport::TestCase
+  def setup
+    @venue = Venue.new(email: "email@email.com", password: "password", password_confirmation: "password",
+                       name: "EFS", location: "Toronto", age_of_entry: "19", capacity: 300,
+                       music: "Top 40/Hip Hop", dress_code: "No runners, dress to impress.",
+                       website: "https://efs.com", business_type: "Club")
+  end
 
+  test "should be valid" do
+    assert @venue.valid?
+  end
+
+  test "name should be present" do
+    @venue.name = ""
+    assert_not @venue.valid?
+  end
+
+  test "age of entry should be present" do
+    @venue.age_of_entry = ""
+    assert_not @venue.valid?
+  end
+
+  test "business type should be present" do
+    @venue.business_type = "      "
+    assert_not @venue.valid?
+  end
 end
